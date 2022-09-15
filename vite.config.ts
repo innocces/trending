581ed01782d7e87b117f9bf2152f88a7032036da
innocces <jiangjiangjiangxi@gmail.com>
2022-09-15 18:58:06 +0800

    docs(gh): fix gh-pages sources 404

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GH ? '/trending/' : '/',
   server: {
     host: true,
     https: true
