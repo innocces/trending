866821325bc7a8b33668aadfb56af47ee0ad2b95
innocces <innocces@users.noreply.github.com>
2022-09-19 12:24:52 +0000

    deploy: 6f18262209749e3d76052b266e345f7436a8a9ab

--- assets/index.0c1cb0f7.js
+++
@@ -1,1 +0,0 @@
-import{j as r,V as a}from"./index.078e0e90.js";const e=()=>r("div",{children:r(a,{to:"/",children:"Search"})});e.displayName="Search";export{e as default};
---
+++ assets/index.4757f03a.js
@@ -0,0 +1,1 @@
+import{j as r,W as a}from"./index.500c4fe8.js";const e=()=>r("div",{children:r(a,{to:"/",children:"Search"})});e.displayName="Search";export{e as default};
